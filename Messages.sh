7d0e78cc405f22108a2a2627132ef4ac47848ca4
Harald Sitter <sitter@kde.org>
2017-01-16 14:43:49 +0100

    comment on why index.html is not extracted

--- Messages.sh
+++ Messages.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Aggregate all files we'll want to assemble into the pot. Skip index.html assemble
+# there are no UI strings meant to be in there and the entire file is meant to be
+# untranslatable.
 file_args=""
 for html_file in `find ubiquity-slideshow -name \*.html | grep -v index.html`; do
   file_args="${file_args} -m ${html_file}"
